c85457eaeed6bbc41114f099f8fe42d9279792a1
Mohammad Durrani <mdurrani808@gmail.com>
2023-12-18 15:10:11 -0500

    gotten example code working

---
+++ bin/CMakeFiles/my-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my-test.dir/src/main.cpp.o"
+  "CMakeFiles/my-test.dir/src/main.cpp.o.d"
+  "my-test"
+  "my-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
